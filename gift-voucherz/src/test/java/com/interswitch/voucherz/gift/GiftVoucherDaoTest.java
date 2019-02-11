3cd45fc83190261cba8bad3a7eb244c38e05146d
lapido <victortemitope95@gmail.com>
2019-02-11 13:57:54 +0100

    Lorem Ipsum

--- gift-voucherz/src/test/java/com/interswitch/voucherz/gift/GiftVoucherDaoTest.java
+++ gift-voucherz/src/test/java/com/interswitch/voucherz/gift/GiftVoucherDaoTest.java
@@ -1,28 +1,28 @@
-package com.interswitch.voucherz.gift;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@ActiveProfiles("test")
-@EnableConfigurationProperties
-@SpringBootTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class GiftVoucherDaoTest {
-
-    @Value("${spring.datasource.url}")
-    private String connectionString;
-
-    @Test
-    public void testIsCreated(){
-        System.out.println(connectionString);
-    }
-}
+//package com.interswitch.voucherz.gift;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.TestPropertySource;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@ActiveProfiles("test")
+//@EnableConfigurationProperties
+//@SpringBootTest
+//@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+//@TestPropertySource(locations = "classpath:application-test.properties")
+//public class GiftVoucherDaoTest {
+//
+//    @Value("${spring.datasource.url}")
+//    private String connectionString;
+//
+//    @Test
+//    public void testIsCreated(){
+//        System.out.println(connectionString);
+//    }
+//}
--- gift-voucherz/src/test/java/com/interswitch/voucherz/gift/service/GiftVoucherServiceTest.java
+++ gift-voucherz/src/test/java/com/interswitch/voucherz/gift/service/GiftVoucherServiceTest.java
@@ -1,24 +1,24 @@
-package com.interswitch.voucherz.gift.service;
-
-import com.interswitch.voucherz.gift.api.service.GiftVoucherService;
-import com.microsoft.sqlserver.jdbc.SQLServerException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class GiftVoucherServiceTest {
-    @Autowired
-    GiftVoucherService giftVoucherService;
-
-    @Test
-    public void create() throws SQLServerException {
-
-    }
-
-
-
-}
+//package com.interswitch.voucherz.gift.service;
+//
+//import com.interswitch.voucherz.gift.api.service.GiftVoucherService;
+//import com.microsoft.sqlserver.jdbc.SQLServerException;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+//public class GiftVoucherServiceTest {
+//    @Autowired
+//    GiftVoucherService giftVoucherService;
+//
+//    @Test
+//    public void create() throws SQLServerException {
+//
+//    }
+//
+//
+//
+//}
